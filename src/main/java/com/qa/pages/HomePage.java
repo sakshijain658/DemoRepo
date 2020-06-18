dee643ef463a211d4fc742a293b7e49805b8a568
Your Name <you@example.com>
2020-06-18 16:11:49 +1000

    firset code in selenium

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class HomePage {
+ public void HomePage(){
+	 System.out.println("homepage method");
+ }
+}
---
+++ src/test/java/com/qa/test/LoginTest.java
@@ -0,0 +1,9 @@
+package com.qa.test;
+
+public class LoginTest {
+
+	 @org.testng.annotations.Test
+	 public void Test(){
+		 System.out.println("test1");
+	 }
+}
